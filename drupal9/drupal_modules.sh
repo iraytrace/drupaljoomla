b40b09d20e9f6c77171cbe817c25d5a67c31f77b
iraytrace <iraytrace@me.com>
2024-09-13 01:34:11 +0000

    add .gitignore

---
+++ drupal9/drupal_modules.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+composer require 'drupal/admin_toolbar:^3.5'
+composer require 'drupal/view_custom_table:^2.0'
+composer require 'drupal/layout_bg:^1.4'
+
+composer require drupal/core-recommended:10.3.4 drupal/core-composer-scaffold:10.3.4 drupal/core-project-message:10.3.4 --update-with-all-dependencies
