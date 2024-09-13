df9af2dccaa5cce2fdf3bfb9d682368972d0f246
iraytrace <iraytrace@me.com>
2024-09-13 01:54:06 +0000

    update dupal_modules.sh to reflect views_custom_tables:2.0.5 is last version to support drupal9

--- drupal9/drupal_modules.sh
+++ drupal9/drupal_modules.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 composer require 'drupal/admin_toolbar:^3.5'
-composer require 'drupal/view_custom_table:^2.0'
+# for drupal 10 / 11
+# composer require 'drupal/view_custom_table:^2.0'
+# for drupal 9.3+ / 10
+composer require 'drupal/view_custom_table:2.0.5'
 composer require 'drupal/layout_bg:^1.4'
 
 composer require drupal/core-recommended:10.3.4 drupal/core-composer-scaffold:10.3.4 drupal/core-project-message:10.3.4 --update-with-all-dependencies
