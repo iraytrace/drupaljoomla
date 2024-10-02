190bf66eb8eacaad4d8be611ed0f5444408186f1
iraytrace <iraytrace@me.com>
2024-10-02 12:26:59 +0000

    update drupal_modules

--- drupal10/drupal_modules.sh
+++ drupal10/drupal_modules.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 composer require 'drupal/admin_toolbar:^3.5'
+composer require 'drupal/feeds:^3.0@RC'
+composer require 'drupal/menu_items_visibility:^1.1'
+composer require 'drupal/workflow:^1.8'
+composer require 'drupal/workflows_field:^2.1'
+composer require 'drupal/rules:^4.0'
+composer require 'drupal/migrate_tools:^6.0'
+
 # for drupal 10 / 11
 # composer require 'drupal/view_custom_table:^2.0'
 # for drupal 9.3+ / 10
 #composer require 'drupal/view_custom_table:2.0.5'
 #composer require 'drupal/layout_bg:^1.4'
-composer require 'drupal/menu_items_visibility:^1.1'
-composer require 'drupal/feeds:^3.0@RC'
 
-composer require drupal/core-recommended:10.3.4 drupal/core-composer-scaffold:10.3.4 drupal/core-project-message:10.3.4 --update-with-all-dependencies
+# composer require drupal/core-recommended:10.3.5 drupal/core-composer-scaffold:10.3.5 drupal/core-project-message:10.3.5 --update-with-all-dependencies
