60c28ec8c577b7d09df76ed00ee45bc45280ed5e
Lee A. Butler <iraytrace@gmail.com>
2024-10-31 22:59:51 -0400

    make docker backup/restore scripts use timestamps

--- all/backup.sh
+++ all/backup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 DBCONTAINER=`docker ps | awk '/mariadb/ {print $1}'`
+TS=$(date '+%Y_%m_%d_%H%M-%S')
 
 docker exec ${DBCONTAINER} sh -c 'mysqldump --user=drupal --password=drupal drupal > /site_db_backup.sql'
-docker cp ${DBCONTAINER}:site_db_backup.sql ./site_db_backup.sql
+docker cp ${DBCONTAINER}:site_db_backup.sql ./site_${TS}.sql
 
 
 DRUPALCONTAINER=`docker ps | awk '/drupal-1/ {print $1}'`
 docker exec -it ${DRUPALCONTAINER}  tar -C /opt/drupal -cjf /drupal.tbz .
-docker cp ${DRUPALCONTAINER}:/drupal.tbz ./site_drupal_files.tbz
+docker cp ${DRUPALCONTAINER}:/drupal.tbz ./site_${TS}.tbz
 
--- all/restore.sh
+++ all/restore.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-
-if [ -f site_db_backup.sql ] ; then
-	DBCONTAINER=`docker ps | awk '/mariadb/ {print $1}'`
-	echo docker cp site_db_backup.sql ${DBCONTAINER}:site_db_backup.sql
-	docker cp site_db_backup.sql ${DBCONTAINER}:site_db_backup.sql
-	echo docker exec ${DBCONTAINER} sh -c 'mysql --user=drupal --password=drupal drupal < /site_db_backup.sql'
-	docker exec ${DBCONTAINER} sh -c 'mysql --user=drupal --password=drupal drupal < /site_db_backup.sql'
-else
-	echo no site_db_backup.sql to restore
-	exit -1
+if [ $# -ne 1 ] ; then
+        echo usage $0 Timestamp
+        exit -1
 fi
 
-if [ -f site_drupal_files.tbz ] ; then
+TS=$1
+
+if [ -f site_${TS}.sql -a -f site_${TS}.tbz ] ; then
+	DBCONTAINER=`docker ps | awk '/mariadb/ {print $1}'`
+	echo docker cp site_${TS}.sql ${DBCONTAINER}:site_db_backup.sql
+	docker cp site_${TS}.sql ${DBCONTAINER}:site_db_backup.sql
+	echo docker exec ${DBCONTAINER} sh -c 'mysql --user=drupal --password=drupal drupal < /site_db_backup.sql'
+	docker exec ${DBCONTAINER} sh -c 'mysql --user=drupal --password=drupal drupal < /site_db_backup.sql'
+
 	DRUPALCONTAINER=`docker ps | awk '/drupal-1/ {print $1}'`
-	echo docker cp site_drupal_files.tbz ${DRUPALCONTAINER}:/site_drupal_files.tbz
-	docker cp site_drupal_files.tbz ${DRUPALCONTAINER}:/site_drupal_files.tbz
+	echo docker cp site_${TS}.tbz ${DRUPALCONTAINER}:/site_drupal_files.tbz
+	docker cp site_${TS}.tbz ${DRUPALCONTAINER}:/site_drupal_files.tbz
 	echo docker exec -it ${DRUPALCONTAINER} tar -C /opt/drupal -xaf /site_drupal_files.tbz
 	docker exec -it ${DRUPALCONTAINER} tar -C /opt/drupal -xaf /site_drupal_files.tbz
 else
