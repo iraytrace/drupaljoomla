fe8d3278005fc2fac8ce7afb00dad9ab2c44a177
Lee A. Butler <iraytrace@gmail.com>
2024-11-03 17:42:08 -0500

    convert db-csv.sh to db-tsv.sh

---
+++ all/db-tsv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -ne 1 ] ; then
+
+	echo "Usage $0 dbname"
+	exit -1
+fi
+DBCONTAINER=`docker ps | awk '/mariadb/ {print $1}'`
+docker cp $1 $DBCONTAINER:/db.sql
+docker cp db_ingest_and_dump_tsv.sh $DBCONTAINER:/db_ingest_and_dump_tsv.sh
+docker exec -it $DBCONTAINER /db_ingest_and_dump_tsv.sh /db.sql
+docker cp $DBCONTAINER:/tables.tgz ./tables.tgz
