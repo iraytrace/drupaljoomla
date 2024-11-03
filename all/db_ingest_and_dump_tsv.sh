1005bfc82d345c4f7e9a0101d9070b374d29d703
Lee A. Butler <iraytrace@gmail.com>
2024-11-03 17:39:51 -0500

    convert db_ingest_and_dump_csv.sh to tsv format

---
+++ all/db_ingest_and_dump_tsv.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -ne 2 ] ; then
+	echo "Usage $0 input.sql"
+	exit -1
+fi
+
+# MySQL credentials
+DB_USER="$DB_USER"
+DB_PASSWORD="$DB_PASSWORD"
+DB_NAME="ingest"
+
+mysql -u$DB_USER -p$DB_PASSWORD -e'create database ingest;'
+mysql -u$DB_USER -p$DB_PASSWORD $DB_NAME < $1
+TABLES=$(awk 'DROP TABLE/ {print $5}' < $1
+
+# Directory to store table dumps
+OUTPUT_DIR="./tables"
+if [ ! -d $OUTPUT_DIR ] ; then
+	mkdir -p $OUTPUT_DIR
+fi
+
+for TABLE in $TABLES ; do
+	echo "SELECT * from $TABLE" | mysql -B -u$DB_USER -p$DB_PASSWORD $DB_NAME > ${TABLE}.tsv
+done
+mysql -u$DB_USER -p$DB_PASSWORD -e "drop database $DB_NAME;"
+tar cvf tables.tbz $OUTPUT_DIR
--- local/install_local.sh
+++ local/install_local.sh
@@ -4,5 +4,4 @@ cd /c/xampp/htdocs
 composer create-project 'drupal/recommended-project:^10' jcehdms
 cd jcehdms
 . /c/users/butler/Documents/src/drupaljoomla/all/drupal_modules.sh
-cp -r /c/users/butler/Documents/src/
 tar -xaf ~/Documents/src/Drupal/Tarapro-Drupal-Theme-ul/tarapro.tar -C /c/xampp/htdocs/jcehdms/web/themes
