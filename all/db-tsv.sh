67b397b00bf8e9e4fecac156f23c9c717ba2d745
Lee A. Butler <iraytrace@gmail.com>
2024-11-03 18:39:52 -0500

    fix typo in db-tsv.sh

--- all/db-tsv.sh
+++ all/db-tsv.sh
@@ -9,4 +9,4 @@ DBCONTAINER=`docker ps | awk '/mariadb/ {print $1}'`
 docker cp $1 $DBCONTAINER:/db.sql
 docker cp db_ingest_and_dump_tsv.sh $DBCONTAINER:/db_ingest_and_dump_tsv.sh
 docker exec -it $DBCONTAINER /db_ingest_and_dump_tsv.sh /db.sql
-docker cp $DBCONTAINER:/tables.tgz ./tables.tgz
+docker cp $DBCONTAINER:/tables.tgz ./tables.tbz
