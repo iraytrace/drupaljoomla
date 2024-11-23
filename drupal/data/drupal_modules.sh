0487c9e8488da81e0b32a40d53e030b9bba0b889
Lee A. Butler <iraytrace@gmail.com>
2024-11-22 21:20:19 -0500

    update drupal_modules.sh

--- drupal/data/drupal_modules.sh
+++ drupal/data/drupal_modules.sh
@@ -4,14 +4,14 @@ composer require 'drupal/admin_toolbar:^3.5'
 composer require 'drupal/auto_entitylabel:^3.3'
 composer require 'drupal/feeds:^3.0@RC'
 composer require 'drupal/menu_items_visibility:^1.1'
-composer require 'drush/drush'
+sudo composer require 'drush/drush'
 
 composer require 'drupal/view_custom_table:^2.0'
 
 composer require 'drupal/taxonomy_import:^2.0'
-composer require 'drupal/eca' 'drupal/bpmn_io'
-composer require 'drupal/eca_tamper'
-composer require 'drupal/eca_content_access:^1.0'
+#composer require 'drupal/eca' 'drupal/bpmn_io'
+#composer require 'drupal/eca_tamper'
+#composer require 'drupal/eca_content_access:^1.0'
 
 composer require 'drupal/workflow:^1.8'
 composer require 'drupal/workflows_field:^2.1'
