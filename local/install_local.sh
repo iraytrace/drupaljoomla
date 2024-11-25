879fd7d3de835e7b27e16ca972166008ba581115
Lee A. Butler <iraytrace@gmail.com>
2024-11-25 08:57:03 -0500

    update install_local.sh

--- local/install_local.sh
+++ local/install_local.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-if [ $# -ge 1 ] ; then
-SITE=$1
-else
-SITE=jcehdms
+
+if [ $# -lt 1 ] ; then
+	echo "usage $0 sitename"
+	exit -1
 fi
-echo ${SITE}
+SITE=$1
+
+DB_USER=root
+DB_PASS=example
+
+export PATH=$PATH:${DRUPAL}/vendor/bin:/c/xampp/mysql/bin
+echo "DROP DATABASE IF EXISTS $SITE; CREATE DATABASE $SITE;" | mysql -u $DB_USER -p$DB_PASS
 
 cd /c/xampp/htdocs
 composer create-project 'drupal/recommended-project:10.3.5' ${SITE}
 cd ${SITE}
+
+cat > .gitignore << EOF
+/vendor/
+web/core
+web/modules/contrib/
+web/themes/contrib/
+web/sites/*/services*.yml
+
+web/sites/*/files
+web/sites/*/private
+web/sites/simpletest
+EOF
+
+git init .
+git add .
+git commit -m 'Initial'
+mkdir -p config/sync
+
 . ${SCRIPT_DIR}/../drupal/data/drupal_modules.sh
